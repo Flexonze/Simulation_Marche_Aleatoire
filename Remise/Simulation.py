a1ede435dfe66db51c01ae74e2792cc349f72e0c
Félix Gravel <Flexonze@gmail.com>
2019-12-05 00:38:05 -0500

    Cleanup + interface + generation d'un csv pour S de 1 a 500

--- Remise/Simulation.py
+++ Remise/Simulation.py
@@ -2,12 +2,32 @@ from marche import Marche
 from modele import Modele
 from tkinter import *
 import statistics
+import csv
+
+def generate_one_to_x_csv(number_of_steps, executions, walk_type):
+    # Création d'un csv contenant la moyenne des distances au carrée de (executions) marches pour un nombre de pas allant de 1 jusqu'à (number_of_steps)
+    averages = []
+    for x in range(number_of_steps):
+        distances = []
+        for i in range(executions):
+            marche = Marche()
+            marche.generate_z_steps(x, walk_type)
+            distances.append(marche.get_distance())
+
+        # Imprime la moyenne
+        average = statistics.mean(distances)
+        print(average)
+        averages.append(average)
+
+    with open(f'{walk_type}_1_to_{number_of_steps}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(averages)
+        print("fin")
 
 def main():
-    distances = []
-    executions = 100000
-    number_of_steps = 50
-    walk_type = 'C' # 'C', 'S', 'U'
+    executions = 10000
+    number_of_steps = 500
+    walk_type = 'S' # 'C', 'S', 'U'
 
     # Génère une marche pour le render
     marche = Marche()
@@ -16,12 +36,19 @@ def main():
     modele = Modele(marche.get_steps(), master=window)
     modele.run()
 
+    # Génère et calcule la moyenne des distances au carrée des (executions) marches de (number_of_steps) pas
+    distances = []
     for i in range(executions):
         marche = Marche()
         marche.generate_z_steps(number_of_steps, walk_type)
         distances.append(marche.get_distance())
 
+    # Imprime la moyenne
     print(statistics.mean(distances))
+
+
+    # Création d'un csv contenant la moyenne des distances au carrée de (executions) marches pour un nombre de pas allant de 1 jusqu'à (number_of_steps)
+    # generate_one_to_x_csv(number_of_steps, executions, walk_type)
 
     # Debugs #
     # marche.display_walk() # Debug
--- Remise/modele.py
+++ Remise/modele.py
@@ -1,18 +1,13 @@
-""" Renderin d'un modele avec Tkinter. """
+import math
 import sys
-from tkinter import *
 from time import sleep
-
-doRenderTk=True # choix de faire un rendering Tk ou pas
-step_length = 10
-offset = 500
+from tkinter import *
 
 class Modele():
-    """ Modele pour simulation. """
-  
-    def __init__(self, steps, master=None):
-        #self.init_modele() # init du modele lui-meme
-        # canvas pour le rendering graphique 
+
+    def __init__(self, steps, step_length=10, refresh_rate=0.05, master=None):
+        width=1000
+        height=1000
         self.steps = steps
         self.state = 0
         self.current_step = self.steps[self.state]
@@ -22,33 +17,83 @@ class Modele():
             self.g = None
             return
         
-        self.refreshTk=0.05
+        self.refreshTk=refresh_rate
         self.waitTk=3
         self.frame = Frame(master)
         self.frame.pack()
         self.bframe = Frame(self.frame)
         self.bframe.pack(side=TOP)
         self.canvas_frame = Frame(self.frame, bd=2, relief=RAISED)
-        self.canvas=Canvas(self.canvas_frame, bg='white', width=1000, height=1000) 
+        self.canvas=Canvas(self.canvas_frame, bg='white', width=width, height=height) 
         self.canvas.pack()
         self.canvas_frame.pack(side=TOP)
         self.canvas.delete(ALL)
+        self.offset = width // 2
+        self.step_length = step_length
 
-    # update du modele 
+        # Écriture initial dans le canvas
+        self.step_text = f'Step {self.state}'
+        self.canvas_step_text = self.canvas.create_text(55, 20, fill="black", font="monaco", text=self.step_text)
+
+        self.xy_text = f'0'
+        self.canvas_xy_text = self.canvas.create_text(150, 40, fill="black", font="monaco", text=self.xy_text)
+        self.circle = None
+
+        # Point bleu en [0, 0]
+        self.canvas.create_oval(
+            0 + self.offset,
+            0 + self.offset,
+            0 + self.offset,
+            0 + self.offset,
+            outline="blue",
+            fill="blue", 
+            width=5,
+        )
+
+    # Update du modele 
     def update(self):
         self.previous_step = self.current_step
         self.state = self.state + 1 
         if self.state < len(self.steps):
             self.current_step = self.steps[self.state]
+            self.step_text = f'Step {self.state}'
+            self.xy_text = f'Current position: {self.current_step}'
 
-    # rendering du modele dans le canvas Tk
+    # Rendering du modele dans le canvas
     def render(self, canvas):
         canvas.create_line(
-            self.previous_step[0] * step_length + offset,
-            self.previous_step[1] * step_length + offset,
-            self.current_step[0] * step_length + offset,
-            self.current_step[1] * step_length + offset,
+            self.previous_step[0] * self.step_length + self.offset,
+            self.previous_step[1] * self.step_length + self.offset,
+            self.current_step[0] * self.step_length + self.offset,
+            self.current_step[1] * self.step_length + self.offset,
         )
+
+        if self.circle != None:
+            canvas.delete(self.circle)
+
+        self.circle = canvas.create_oval(
+                self.current_step[0] * self.step_length + self.offset,
+                self.current_step[1] * self.step_length + self.offset,
+                self.current_step[0] * self.step_length + self.offset,
+                self.current_step[1] * self.step_length + self.offset,
+                outline="red",
+                fill="red", 
+                width=5,
+        )
+
+        canvas.itemconfigure(self.canvas_step_text, text=self.step_text)
+        canvas.itemconfigure(self.canvas_xy_text, text=self.xy_text)
+    
+        if self.state == len(self.steps):
+            canvas.create_line(
+                0 + self.offset,
+                0 + self.offset,
+                self.current_step[0] * self.step_length + self.offset,
+                self.current_step[1] * self.step_length + self.offset,
+                dash=(4, 4),
+                fill="red"
+            )
+            self.canvas.create_text(112, 60, fill="red", font="monaco", text=f'Final distance: {pow(self.current_step[0], 2) + pow(self.current_step[1], 2)}')
 
     def run(self):
         for step in self.steps:
@@ -57,6 +102,7 @@ class Modele():
 
             if self.canvas is not None:                                                                      
                 self.render(self.canvas)                                                                
-                self.canvas.update(); sleep(self.refreshTk)
+                self.canvas.update()
+                sleep(self.refreshTk)
 
         sleep(int(5))
