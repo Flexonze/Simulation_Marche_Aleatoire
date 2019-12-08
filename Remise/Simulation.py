cd5918291ce6a83c274cfb35fde7ccee0c7d926a
Félix Gravel <Flexonze@gmail.com>
2019-12-07 19:58:25 -0500

    cleanup

--- Remise/Simulation.py
+++ Remise/Simulation.py
@@ -4,14 +4,17 @@ from tkinter import *
 import statistics
 import csv
 
+
 def localize_floats(row):
+    """ Fontion d'aide pour créer un csv français """
     return [
         str(el).replace('.', ',') if isinstance(el, float) else el 
         for el in row
     ]
 
+
 def generate_one_to_x_csv(number_of_steps, executions, walk_type):
-    # Création d'un csv contenant la moyenne des distances au carrée de (executions) marches pour un nombre de pas allant de 1 jusqu'à (number_of_steps)
+    """ Création d'un csv contenant la moyenne des distances au carrée de (executions) marches pour un nombre de pas allant de 1 jusqu'à (number_of_steps) """
     averages = []
     fails = []
     for x in range(number_of_steps + 1):
@@ -46,70 +49,42 @@ def generate_one_to_x_csv(number_of_steps, executions, walk_type):
 
     print("fin")
 
-def main():
-    executions = 1
-    number_of_steps = 150
-    walk_type = 'U' # 'C', 'S', 'U'
+
+def render_walk(number_of_steps, walk_type):
+    """ Génère une marche pour le rendering """ 
+    marche = Marche()
+    failed = marche.generate_z_steps(number_of_steps, walk_type)
+
+    while failed:
+        marche.failed = False
+        failed = marche.generate_z_steps(number_of_steps, walk_type)
+
+    window = Tk(); window.geometry("+0+0"); window.title("simulation")
+    modele = Modele(marche.get_steps(), master=window)
+    modele.run()
 
 
-    ##### Génère une marche pour le render ##############################
-    #marche = Marche()
-    #failed = marche.generate_z_steps(number_of_steps, walk_type)
+def generate_walks(executions, number_of_steps, walk_type):
+    """ Génère (executions) marches de (number_of_steps) pas de type (walk_type) """
+    distances = []
+    fail_count = 0
 
-    #while failed:
-    #    marche.failed = False
-    #    failed = marche.generate_z_steps(number_of_steps, walk_type)
+    for i in range(executions):
+        marche = Marche()
+        failed = marche.generate_z_steps(number_of_steps, walk_type)
+       
+        while failed:
+            fail_count = fail_count + 1
+            marche.failed = False
+            failed = marche.generate_z_steps(number_of_steps, walk_type)
 
-    #window = Tk(); window.geometry("+0+0"); window.title("simulation")
-    #modele = Modele(marche.get_steps(), master=window)
-    #modele.run()
-    #####################################################################
+        distances.append(marche.get_distance())
 
+    print(statistics.mean(distances))
+    print(f'fails : {fail_count}')
 
-    # Génère et calcule la moyenne des distances au carrée des (executions) marches de (number_of_steps) pas#
-    #distances = []
-    #fail_count = 0
-    #for i in range(executions):
-    #   marche = Marche()
-    #   failed = marche.generate_z_steps(number_of_steps, walk_type)
-
-    #    while failed:
-    #        fail_count = fail_count + 1
-    #        marche.failed = False
-    #        failed = marche.generate_z_steps(number_of_steps, walk_type)
-
-    #    distances.append(marche.get_distance())
-
-     # Imprime la moyenne
-    #print(statistics.mean(distances))
-    #print(f'fails : {fail_count}')
-    ##########################################################################################################
-
-    # Création d'un csv contenant la moyenne des distances au carrée de (executions) marches pour un nombre de pas allant de 1 jusqu'à (number_of_steps)
-    # generate_one_to_x_csv(number_of_steps, executions, walk_type)
-
-
-    ############ Partie 2 @###################################################################################
-    ##########################################################################################################
-
-    # Render 
-    #marche = Marche()
-    #failed = marche.generate_z_steps(number_of_steps, walk_type)
-
-    #while failed:
-    #    marche.failed = False
-    #    failed = marche.generate_z_steps(number_of_steps, walk_type)
-
-    #window = Tk(); window.geometry("+0+0"); window.title("simulation")
-    #modele = Modele(marche.get_steps(), master=window)
-    #modele.run()
-    ################
-
-
-    executions = 1
-    number_of_minutes = 150 # 1 step per minute on speed 1
-    walk_type = 'S' # 'C', 'S', 'U'
-
+def generate_martian_walks(executions, number_of_minutes, walk_type):
+    """ Génère (executions) marches de (number_of_minutes) minutes de type (walk_type) """
     distances = []
     fail_count = 0
     for i in range(executions):
@@ -128,7 +103,28 @@ def main():
     print(f'fails : {fail_count}')
     print(f'meters : {len(marche.get_steps()) - 1}')
 
-    ###########################################################################################################
+def main():
+    executions = 1
+    number_of_steps = 150
+    walk_type = 'S' # 'C', 'S', 'U'
+
+    ## Render une marche ##
+    render_walk(number_of_steps, walk_type)
+
+    ## Génère (executions) marches de (number_of_steps) pas de type (walk_type) ##
+    generate_walks(executions, number_of_steps, walk_type)
+
+    ## Création d'un csv ##
+    # generate_one_to_x_csv(number_of_steps, executions, walk_type)
+
+    #### Partie 2 ####
+    executions = 1
+    number_of_minutes = 150 # 1 step per minute on speed 1
+    walk_type = 'U' # 'C', 'S', 'U'
+
+    ## Génère (executions) marches de (number_of_minutes) minutes de type (walk_type) ##
+    # generate_martian_walks(executions, number_of_minutes, walk_type)
+
 
 
     return
